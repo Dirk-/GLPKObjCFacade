727c9f33e126ca6c7d6135ab4c855e7aa662f67d
Dirk Fröhling <dirk.froehling@w-hs.de>
2018-06-23 17:16:04 +0200

    Added iOS target

---
+++ GLPKObjCFacade_iOS/GLPKObjCFacade_iOS.h
@@ -0,0 +1,19 @@
+//
+//  GLPKObjCFacade_iOS.h
+//  GLPKObjCFacade_iOS
+//
+//  Created by Dirk Fröhling on 23.06.18.
+//  Copyright © 2018 Dirk Fröhling IT-Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GLPKObjCFacade_iOS.
+FOUNDATION_EXPORT double GLPKObjCFacade_iOSVersionNumber;
+
+//! Project version string for GLPKObjCFacade_iOS.
+FOUNDATION_EXPORT const unsigned char GLPKObjCFacade_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLPKObjCFacade_iOS/PublicHeader.h>
+
+

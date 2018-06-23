efd9bca38266e0e8715dccc742d9ea0cb40ec24e
Dirk Fröhling <dirk.froehling@w-hs.de>
2018-06-23 15:54:46 +0200

    Initial Commit

---
+++ GLPKObjCFacade/GLPKObjCFacade.h
@@ -0,0 +1,19 @@
+//
+//  GLPKObjCFacade.h
+//  GLPKObjCFacade
+//
+//  Created by Dirk Fröhling on 23.06.18.
+//  Copyright © 2018 Dirk Fröhling IT-Consulting. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GLPKObjCFacade.
+FOUNDATION_EXPORT double GLPKObjCFacadeVersionNumber;
+
+//! Project version string for GLPKObjCFacade.
+FOUNDATION_EXPORT const unsigned char GLPKObjCFacadeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GLPKObjCFacade/PublicHeader.h>
+
+

4a657591c7c0a572f91de55a921c665f5e839af2
Dirk Fröhling <dirk.froehling@w-hs.de>
2018-06-23 18:21:27 +0200

    Targets renamed

--- GLPKObjCFacade_macOS/GLPKObjCFacade_macOS.h
+++ GLPKObjCFacade_macOS/GLPKObjCFacade_macOS.h
@@ -1,6 +1,6 @@
 //
-//  GLPKObjCFacade.h
-//  GLPKObjCFacade
+//  GLPKObjCFacade_macOS.h
+//  GLPKObjCFacade_macOS
 //
 //  Created by Dirk Fröhling on 23.06.18.
 //  Copyright © 2018 Dirk Fröhling IT-Consulting. All rights reserved.
@@ -9,11 +9,11 @@
 #import <Cocoa/Cocoa.h>
 
 //! Project version number for GLPKObjCFacade.
-FOUNDATION_EXPORT double GLPKObjCFacadeVersionNumber;
+FOUNDATION_EXPORT double GLPKObjCFacade_macOSVersionNumber;
 
 //! Project version string for GLPKObjCFacade.
-FOUNDATION_EXPORT const unsigned char GLPKObjCFacadeVersionString[];
+FOUNDATION_EXPORT const unsigned char GLPKObjCFacade_macOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GLPKObjCFacade/PublicHeader.h>
-#import <GLPKObjCFacade/ObjCGLPK.h>
+#import <GLPKObjCFacade_macOS/ObjCGLPK.h>
 
